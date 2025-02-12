3a91729ddeeb6848b7433df11b551d7eeb9bc12f
Lslipski <lslipski602@gmail.com>
2025-02-11 17:28:52 -0800

    began building 05_financial_information and the load_990_2023 function

---
+++ functions/load_990_2023.R
@@ -0,0 +1,103 @@
+
+
+load_990_2023 <- function() {
+  tribble(~section, ~element, ~value, ~element_name,
+          
+          ############################## PART VIII ##############################          
+          "Part VIII: Statement of Revenue", 
+          "Government Grants",
+          24759761,
+          "1e",
+          
+          "Part VIII: Statement of Revenue", 
+          "All other contributions",
+          2338072,
+          "1f",
+          
+          "Part VIII: Statement of Revenue", 
+          "Net Patient Service Revenue",
+          80606606,
+          "2a",
+          
+          "Part VIII: Statement of Revenue", 
+          "Pharmacy Revenue",
+          10665308,
+          "2b",
+          
+          "Part VIII: Statement of Revenue", 
+          "Incentive / QI Income",
+          3353643,
+          "2c",
+          
+          "Part VIII: Statement of Revenue", 
+          "Other Income",
+          1090018,
+          "2d",
+          
+          "Part VIII: Statement of Revenue", 
+          "Gross Rents",
+          37506,
+          "6a",
+          
+          "Part VIII: Statement of Revenue", 
+          "Gross amount from sales of assets other than inventory",
+          5410000,
+          "7a(i)",
+          
+          "Part VIII: Statement of Revenue", 
+          "Gross amount from sales of assets other than inventory",
+          582570,
+          "7a(ii)",
+          
+          "Part VIII: Statement of Revenue", 
+          "Total Revenue",
+          123044931,
+          "12",
+          
+          ##############################PART IX##############################
+          
+          "Part IX: Statement of Functional Expenses",
+          "Compensation of current officers, directors, trustees, and
+key employees",
+          2027670,
+          "5",
+          
+          "Part IX: Statement of Functional Expenses",
+          "Other Salaries and Wages",
+          65167879 ,
+          "7",
+          
+          "Part IX: Statement of Functional Expenses",
+          "Fees for Legal Services (non-employee) ",
+          254119,
+          "11b",
+          
+          "Part IX: Statement of Functional Expenses",
+          "Fees for Accounting Services (non-employee) ",
+          375631,
+          "11c",
+          
+          "Part IX: Statement of Functional Expenses",
+          "Other Fees (non-employee) ",
+          10790847,
+          "11c",
+          
+          
+          ##############################PART X##############################
+          
+          "Part X: Balance Sheet",
+          1,
+          
+          ##############################PART XI##############################
+          
+          "Part XI: Reconciliation of Net Assets",
+          1,
+          
+          ##############################PART XII##############################
+          
+          "Part XII: Financial Statements and Reporting",
+          1
+          
+  )
+  
+}
